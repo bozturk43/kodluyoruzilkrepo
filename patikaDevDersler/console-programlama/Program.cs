fea278248d929e0eec07c403d1441ac4495cd72e
bozturk43 <92303776+bozturk43@users.noreply.github.com>
2022-04-03 15:32:58 +0300

    Degiskenler ve Veri Tipleri Ödevi Eklendi

---
+++ patikaDevDersler/console-programlama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputvar="";
+            inputvar = Console.ReadLine(); 
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(inputvar); 
+        }
+    }
+}
