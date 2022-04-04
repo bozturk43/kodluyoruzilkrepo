f9083d4b669975e91155d286d4d62e1230242024
bozturk43 <92303776+bozturk43@users.noreply.github.com>
2022-04-04 13:02:17 +0300

    arrays-loops added

---
+++ patikaDevDersler/arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler=new string[5];
+
+            string[] hayvanlar ={"Kedi","Köpek","Kuş","Maymun"};
+            
+            int[] dizi;
+            dizi =new int[5];
+
+            //Dizilere değer atama ve erişim.
+            renkler[0]="Mavi";
+            Console.WriteLine(hayvanlar[0]);
+            dizi[3]=10;
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngülerle Diziler
+            Console.WriteLine("Dizi Boyutunu Girin");
+            int diziboyutu=Convert.ToInt32(Console.ReadLine());
+            int[] array=new int[diziboyutu];
+            int ortalama=0;
+            int toplam=0;
+            for (int i = 0; i < diziboyutu; i++)
+            {
+                Console.WriteLine(i+". elemanı girin");
+                array[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int item in array)
+            {
+                toplam+=item;
+            }
+            ortalama=toplam/diziboyutu;
+            Console.WriteLine(ortalama);
+        }
+    }
+}
