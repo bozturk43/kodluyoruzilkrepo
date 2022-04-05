d77144587dfcdcfe1980a7e269bf1b01ae8ab2db
bozturk43 <92303776+bozturk43@users.noreply.github.com>
2022-04-05 13:12:30 +0300

    methods-added

---
+++ patikaDevDersler/metotlar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erişim belirteci-geri donüş tipi-metod adı(parametreler){
+                //metodun yapmasını istediğimizi işlemler.
+            //}
+
+            int a=5;
+            int b=3;
+            Console.WriteLine(a+b);
+            
+            Console.WriteLine("Sonuc:"+toplama(a,b));
+            Metotlar obje=new Metotlar();
+            obje.EkranaYazdır((a+b).ToString());
+            obje.EkranaYazdır(obje.Arttırvetopla(ref a,ref b).ToString());
+        }
+
+        static int toplama(int x,int y){
+
+            return x+y;
+        }
+
+        
+        
+    }
+
+    class Metotlar{
+        
+        public void EkranaYazdır(string deger){
+            Console.WriteLine("Ekrana Yazdır Metodundan Yazdırılan Deger:"+deger);
+        }
+
+        public int Arttırvetopla(ref int d1 , ref int d2){
+            d1+=1;
+            d2+=1;
+            //ref anahtar kelimesi değişkenlerin adresini işaret eder böylece fonksiyon değişkenlerin kopyasını degil değişkenlerin kendisini değiştirirek bu işlemi gerçekleştirir.
+            return d1+d2;
+        }
+    }
+}
